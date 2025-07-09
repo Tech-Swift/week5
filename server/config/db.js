6548c1cfb3d61f5bd3f441c31e27106c4e01dedb
Ticha23 <mutwirigachanja1@gmail.com>
2025-07-09 20:09:37 +0300

    New update

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_PROdUCTION);
+    const conn = await mongoose.connect(process.env.MONGO_URI_PRODUCTION); // <-- fixed typo
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MONGODB connection error", error.message);
